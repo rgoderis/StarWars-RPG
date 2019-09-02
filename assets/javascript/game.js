5c3e634ef2329d8110e4c62432def25aa03c702e
Ryan Goderis <rgoderis789@gmail.com>
2019-09-02 10:51:42 -0400

    set variables for p1 and p2 attributes as well as array of characters

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var hp1;
+var hp2;
+var userAttack;
+var compAttack;
+
+var characters = [
+            {
+                name: "",
+                img: "",
+                attack: num,
+                counterAttack: num},
+            {
+                name: "",
+                img: "",
+                attack: num,
+                counterAttack: num},
+            {
+                name: "",
+                img: "",
+                attack: num,
+                counterAttack: num},
+            {
+                name: "",
+                img: "",
+                attack: num,
+                counterAttack: num}];
+

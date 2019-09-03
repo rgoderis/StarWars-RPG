f69cbed15fb696fd5765b812e117399272126527
Ryan Goderis <rgoderis789@gmail.com>
2019-09-03 10:47:36 -0400

    set function to display clicked available character under your character and hide the available characters div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 var hp1;
 var hp2;
 var userAttack;
@@ -6,26 +8,49 @@ var compAttack;
 var characters = [
             {
                 name: "Luke Skywalker",
-                img: "",
+                img: "assets/images/luke-skywalker.jpeg",
                 hp: 100,
                 attack: 10,
                 counterAttack: 10},
             {
                 name: "Darth Vader",
-                img: "",
+                img: "assets/images/darth-vader.jpg",
                 hp: 100,
                 attack: 10,
                 counterAttack: 10},
             {
                 name: "Emperor Palpatine",
-                img: "",
+                img: "assets/images/emperor-palpatine.jpg",
                 hp: 100,
                 attack: 10,
                 counterAttack: 10},
             {
                 name: "Obi-Wan Kenobi",
-                img: "",
+                img: "assets/images/obi-wan-kenobi.png",
                 hp: 100,
                 attack: 10,
                 counterAttack: 10}];
+// display character name
+// characters.forEach(displayName)
+// function displayName(character) {
+//     console.log(character.name)
+//     $(".name").text(character.name)
+// }
 
+
+
+$(".character").on("click", function(){
+    console.log(this);
+    $("#userCharacter").append(this)
+    $("#availableCharacters").css("display", "none")
+    // console.log(this.name)
+    // $("#charName").text("character name");
+    // $("#charImg").text("character image");
+    // $("#charHP").text("character hp");
+});
+
+
+
+
+
+});

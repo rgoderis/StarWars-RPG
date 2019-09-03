04ad104ff755fa4d5cde5608c08f3b8340a60bb1
Ryan Goderis <rgoderis789@gmail.com>
2019-09-03 16:37:42 -0400

    cleaned up spacing in game and index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-var hp1;
-var hp2;
-var userAttack;
-var compAttack;
+    var hp1;
+    var hp2;
+    var userAttack;
+    var compAttack;
 
-var characters = [
+    var characters = [
             {
                 name: "Luke Skywalker",
                 img: "assets/images/luke-skywalker.jpeg",
@@ -31,20 +31,23 @@ var characters = [
                 attack: 10,
                 counterAttack: 10}];
 
-// displays available characters from characters array
-characters.forEach(function(char){
-    $("#availableCharacters").append("<div class='col-3'><div class='card text-center character'><div class='card-header name'>"+char.name+"</div><div class='imgBox'><img src="+char.img+" class='img' alt="+char.name+"></div><div class='card-footer hp'>"+char.hp+"</div></div></div>")
-})
+    // displays available characters from characters array
+    characters.forEach(function(char){
+     $("#availableCharacters").append("<div class='col-3'><div class='card text-center character'><div class='card-header name'>"+char.name+"</div><div class='imgBox'><img src="+char.img+" class='img' alt="+char.name+"></div><div class='card-footer hp'>"+char.hp+"</div></div></div>")
+    })
 
 
-// activates when a available character is clicked
-$(".character").on("click", function(){
-    // moves clicked character to the Your Character section
-    $("#userCharacter").append(this)
-    // hides the other available characters that weren't selected
-    $("#availableCharacters").css("display", "none")
-});
+    // activates when a available character is clicked
+    $(".character").on("click", function(){
+    
+        // moves clicked character to the Your Character section
+        $("#userCharacter").append(this);
+ 
+        // moves remaining characters to Avaialble Enemies
 
+        // hides the other available characters that weren't selected
+        $("#availableCharacters").css("display", "none")
+    });
 
 
 

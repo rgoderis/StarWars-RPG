5f72a11ba5701dffaa4ead1bf2ba64f6d20f4e26
Ryan Goderis <rgoderis789@gmail.com>
2019-09-04 14:34:12 -0400

    set it so that once the user selects a character and enemy the click listener stops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,67 @@
+var hp1 = 100;
+var hp2 = 100
+var userAttack;
+var compAttack;
+var isDefeated = false;
+var userSelected = false;
+var enemySelected = false;
+
+var characters = [
+        {
+            name: "Luke Skywalker",
+            img: "assets/images/luke-skywalker.jpeg",
+            hp: 100,
+            attack: 10,
+            counterAttack: 10},
+        {
+            name: "Darth Vader",
+            img: "assets/images/darth-vader.jpg",
+            hp: 100,
+            attack: 10,
+            counterAttack: 10},
+        {
+            name: "Emperor Palpatine",
+            img: "assets/images/emperor-palpatine.jpg",
+            hp: 100,
+            attack: 10,
+            counterAttack: 10},
+        {
+            name: "Obi-Wan Kenobi",
+            img: "assets/images/obi-wan-kenobi.png",
+            hp: 100,
+            attack: 10,
+            counterAttack: 10}];
+
 $(document).ready(function() {
-
-    var hp1;
-    var hp2;
-    var userAttack;
-    var compAttack;
-
-    var characters = [
-            {
-                name: "Luke Skywalker",
-                img: "assets/images/luke-skywalker.jpeg",
-                hp: 100,
-                attack: 10,
-                counterAttack: 10},
-            {
-                name: "Darth Vader",
-                img: "assets/images/darth-vader.jpg",
-                hp: 100,
-                attack: 10,
-                counterAttack: 10},
-            {
-                name: "Emperor Palpatine",
-                img: "assets/images/emperor-palpatine.jpg",
-                hp: 100,
-                attack: 10,
-                counterAttack: 10},
-            {
-                name: "Obi-Wan Kenobi",
-                img: "assets/images/obi-wan-kenobi.png",
-                hp: 100,
-                attack: 10,
-                counterAttack: 10}];
 
     // displays available characters from characters array
     characters.forEach(function(char){
-     $("#availableCharacters").append("<div class='col-3 character'><div class='card text-center'><div class='card-header name'>"+char.name+"</div><div class='imgBox'><img src="+char.img+" class='img' alt="+char.name+"></div><div class='card-footer hp'>"+char.hp+"</div></div></div>")
-    })
+        $("#availableCharacters").append("<div class='col-3 character'><div class='card text-center'><div class='card-header name'>"+char.name+"</div><div class='imgBox'><img src="+char.img+" class='img' alt="+char.name+"></div><div class='card-footer hp'>"+char.hp+"</div></div></div>")
+        });
 
     // activates when a available character is clicked
     $(".character").on("click", function(){
-        
+        if(userSelected === false){
         // moves clicked character to the Your Character section
         $("#userCharacter").append(this);
-        
+
+        // remove .character class from userCharacter div
+        // $(this).removeClass("character")
+
         // moves remaining characters to Avaialble Enemies
-        $("#availableEnemies").append($("#availableCharacters"))
+        $("#availableEnemies").append($("#availableCharacters"))                 
+        userSelected = true;          
+        } else {
+            // moves clicked character to chosenEnemy div
+            $("#chosenEnemy").append(this);
+            enemySelected = true;
+        }
 
-        // hides the other available characters that weren't selected
-        // $("#availableCharacters").css("display", "none")
+        // prevent user from clicking on another character
+        if(enemySelected === true){
+            // should prevent the user from clicking on any other characters
+            $(".character").off("click");
+        }
     });
-
-
-
 
 });
